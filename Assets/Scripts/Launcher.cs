c43becd2bde6305b5189e563a8d8c3da42d42d01
Gillian <gillianbenklifa1@gmail.com>
2021-10-19 18:13:08 +0200

    the last hope

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -1,24 +1,40 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class Launcher : MonoBehaviour
 {
     public static string fileToLoad;
+    public TMP_InputField fieldMap;
+    public GameObject MapSelection;
+
     // Start is called before the first frame update
-    public void LoadScene()
+    public void LoadMap()
     {
         fileToLoad = EditorUtility.OpenFilePanel("Select graph file", "","txt");
         if (fileToLoad != "")
         {
-            SceneManager.LoadScene("Load");
+            MapSelection.GetComponent<ToggleGroup>().SetAllTogglesOff();
+            fieldMap.textComponent.SetText(fileToLoad);
+
+            //SceneManager.LoadScene("Load");
         } 
         else
         {
             throw new Exception("File not found");
+        }
+    }
+
+    public void Update()
+    {
+
+        if (MapSelection.GetComponent<ToggleGroup>().AnyTogglesOn())
+        {
+            fileToLoad = "";
+            fieldMap.textComponent.SetText(fileToLoad);
         }
     }
 }
