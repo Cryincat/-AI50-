d822880474138101b9a5db562c7495c7e7c4264b
Gillian <gillianbenklifa1@gmail.com>
2021-12-06 21:10:36 +0100

    creation scene principale + modif HUD

--- Assets/Scripts/ButtonsHUD.cs
+++ Assets/Scripts/ButtonsHUD.cs
@@ -9,15 +9,21 @@ public class ButtonsHUD : MonoBehaviour
 {
 
     public TextMeshProUGUI timerText;
+    public TextMeshProUGUI maxIdleValue;
+    public TextMeshProUGUI averageIdleValue;
     public float timer;
     public float speed = 1;
     public bool playing;
     public Image buttonPausePlay;
     public Sprite pauseImage;
     public Sprite playImage;
+
+    private DataManager dataM;
+
     // Start is called before the first frame update
     void Start()
     {
+        dataM = FindObjectOfType<DataManager>();
         playing = true;
         timer = 0;
         speed = 1;
@@ -31,6 +37,8 @@ public class ButtonsHUD : MonoBehaviour
         string minutes = Mathf.Floor((timer % 3600) / 60).ToString("00");
         string seconds = (timer % 60).ToString("00");
         timerText.text = minutes + ":" + seconds;
+        maxIdleValue.text = (Mathf.Round(dataM.maxIdlenessTheoric)).ToString(); // Recupérer l idleness max
+        averageIdleValue.text = (Mathf.Round(dataM.mediumIdleness)).ToString(); // Recuperer l idleness moyenne
     }
 
     public void clickPlay()
@@ -61,6 +69,15 @@ public class ButtonsHUD : MonoBehaviour
     public void clickx2()
     {
         speed = 2;
+        if (Time.timeScale != 0)
+        {
+            Time.timeScale = speed;
+        }
+    }
+
+    public void clickx5()
+    {
+        speed = 5;
         if (Time.timeScale != 0)
         {
             Time.timeScale = speed;
